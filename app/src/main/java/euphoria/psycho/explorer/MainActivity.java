b7bdc7bdaf4c25459483e60139c2c9b593bb085e
grandiloquent <1785907865@qq.com>
2021-08-07 00:32:47 +0800

    整理代码

--- app/src/main/java/euphoria/psycho/explorer/MainActivity.java
+++ app/src/main/java/euphoria/psycho/explorer/MainActivity.java
@@ -183,7 +183,7 @@ public class MainActivity extends Activity implements ClientInterface {
         return mBookmarkDatabase;
     }
 
-    // 
+    //
     private void initialize() {
         /*
         new Thread(() -> {
