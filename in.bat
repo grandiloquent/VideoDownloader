142c52a1c37e4ef0b9c25c05b0b601f478804b92
grandiloquent <1785907865@qq.com>
2021-08-31 17:00:17 +0800

    更新

--- in.bat
+++
@@ -1,7 +0,0 @@
-echo "# vanilla-html5-video-player" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin git@github.com:grandiloquent/VideoBrowser.git
-git push -u origin main
--- push.bat
+++
@@ -1,7 +0,0 @@
-
-git add .
-git commit -m "changed"
-git push
-
-git config --global user.email "1785907865@qq.com"
-git config --global user.name "grandiloquent"
