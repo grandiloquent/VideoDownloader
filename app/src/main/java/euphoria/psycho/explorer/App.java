0ece783bc0a49d546d85699fc2df216e1b2e62d0
grandiloquent <1785907865@qq.com>
2021-09-01 09:20:54 +0800

    更新

--- app/src/main/java/euphoria/psycho/explorer/App.java
+++ app/src/main/java/euphoria/psycho/explorer/App.java
@@ -8,7 +8,6 @@ import android.os.StrictMode;
 import java.io.File;
 
 import euphoria.psycho.share.Logger;
-import euphoria.psycho.tasks.VideoHelper;
 import euphoria.psycho.utils.FileLog;
 
 public class App extends Application {
@@ -26,14 +25,13 @@ public class App extends Application {
         StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
         StrictMode.setVmPolicy(builder.build());
         //FileShare.initialize(this);
-        VideoHelper.deleteVideoDirectory(this);
+        // VideoHelper.deleteVideoDirectory(this);
         File logDir = new File(getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS), "log");
         if (!logDir.exists()) {
             logDir.mkdirs();
         }
         Logger.d(logDir.getAbsolutePath());
         FileLog.setDir(logDir);
-
         // adb pull /storage/emulated/0/Android/data/euphoria.psycho.explorer/files/Documents/log/. log
     }
 }
