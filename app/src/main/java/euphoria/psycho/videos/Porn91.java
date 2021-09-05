65a7305443392d8c2767d2c95972e52cdd053c44
grandiloquent <1785907865@qq.com>
2021-09-05 08:31:34 +0800

    更新

--- app/src/main/java/euphoria/psycho/videos/Porn91.java
+++ app/src/main/java/euphoria/psycho/videos/Porn91.java
@@ -9,7 +9,6 @@ import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 import euphoria.psycho.explorer.MainActivity;
-import euphoria.psycho.share.Logger;
 import euphoria.psycho.tasks.VideoActivity;
 import euphoria.psycho.tasks.VideoService;
 
@@ -24,6 +23,24 @@ public class Porn91 extends BaseExtractor<String> {
         super(inputUri, mainActivity);
     }
 
+    public void fetchVideoList(String uri) {
+        new Thread(() -> {
+            String response = getString(uri, null);
+            if (response == null) {
+                return;
+            }
+            Matcher matcher = MATCH_91PORN.matcher(response);
+            List<String> videoList = new ArrayList<>();
+            while (matcher.find()) {
+                videoList.add(matcher.group());
+            }
+            startVideoService(mMainActivity, videoList.parallelStream()
+                    .map(VideosHelper::extract91PornVideoAddress)
+                    .collect(Collectors.toList()));
+        }).start();
+
+    }
+
     public static boolean handle(String uri, MainActivity mainActivity) {
         Pattern pattern = Pattern.compile("91porn.com/view_video.php\\?viewkey=[a-zA-Z0-9]+");
         if (pattern.matcher(uri).find()) {
@@ -33,11 +50,19 @@ public class Porn91 extends BaseExtractor<String> {
         return false;
     }
 
+
+    static void startVideoService(MainActivity mainActivity, List<String> videoList) {
+        mainActivity.runOnUiThread(() -> {
+            Intent service = new Intent(mainActivity, VideoActivity.class);
+            service.putExtra(VideoService.KEY_VIDEO_LIST, videoList.toArray(new String[0]));
+            mainActivity.startActivity(service);
+        });
+    }
+
     //
     @Override
     protected String fetchVideoUri(String uri) {
         return VideosHelper.extract91PornVideoAddress(uri);
-        //return new String(buffer, 0, result);
     }
 
     @Override
@@ -48,30 +73,5 @@ public class Porn91 extends BaseExtractor<String> {
     @Override
     protected void processVideo(String videoUri) {
         viewVideoBetter(mMainActivity, videoUri);
-    }
-
-    public void fetchVideoList(String uri) {
-        new Thread(() -> {
-            String response = getString(uri, null);
-            if (response == null) {
-                Logger.d(String.format("'%s' is null.", "response"));
-            }
-            Matcher matcher = MATCH_91PORN.matcher(response);
-            List<String> videoList = new ArrayList<>();
-            while (matcher.find()) {
-                videoList.add(matcher.group());
-            }
-            videoList = videoList.parallelStream()
-                    .map(VideosHelper::extract91PornVideoAddress)
-                    .collect(Collectors.toList());
-            List<String> finalVideoList = videoList;
-            mMainActivity.runOnUiThread(() -> {
-                Intent service = new Intent(mMainActivity, VideoActivity.class);
-                service.putExtra(VideoService.KEY_VIDEO_LIST, finalVideoList.toArray(new String[0]));
-                mMainActivity.startActivity(service);
-            });
-        }).start();
-
-
     }
 }
