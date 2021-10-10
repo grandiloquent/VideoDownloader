6fc5048476d85f9a0623df986e1aecca7a55f100
grandiloquent <1785907865@qq.com>
2021-10-11 00:29:09 +0800

    修正腾讯视频逻辑

--- app/src/main/java/euphoria/psycho/explorer/MainActivity.java
+++ app/src/main/java/euphoria/psycho/explorer/MainActivity.java
@@ -5,7 +5,6 @@ import android.content.Context;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Process;
-import android.util.Log;
 import android.webkit.WebView;
 import android.widget.Toast;
 
@@ -83,13 +82,13 @@ public class MainActivity extends Activity implements ClientInterface {
 //        new Iqiyi("https://www.iqiyi.com/v_19rrok4nt0.html", this)
 //                .parsingVideo();
         //Log.e("B5aOx2", String.format("initialize, %s", getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS)));
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                String uri = Native.fetchQQ("https://m.v.qq.com/x/m/play?cid=znda81ms78okdwd&vid=&ptag=v_qq_com%23v.index.adaptor%233");
-                Log.e("B5aOx2", String.format("run, %s", uri));
-            }
-        }).start();
+//        new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                String[] uri = Native.fetchQQ("https://m.v.qq.com/x/m/play?cid=znda81ms78okdwd&vid=&ptag=v_qq_com%23v.index.adaptor%233");
+//                Log.e("B5aOx2", String.format("run, %s", uri[0]));
+//            }
+//        }).start();
         checkUpdate();
 //        DownloaderTask downloaderTask = new DownloaderTask();
 //        downloaderTask.Directory = DownloaderService.createVideoDownloadDirectory(this).getAbsolutePath();
--- app/src/main/java/euphoria/psycho/videos/QQ.java
+++ app/src/main/java/euphoria/psycho/videos/QQ.java
@@ -1,21 +1,16 @@
 package euphoria.psycho.videos;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+import android.content.Intent;
 
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import euphoria.psycho.explorer.MainActivity;
-import euphoria.psycho.share.Logger;
-import euphoria.psycho.share.StringShare;
+import euphoria.psycho.explorer.Native;
+import euphoria.psycho.player.IqiyiActivity;
+import euphoria.psycho.player.VideoActivity;
 
-import static euphoria.psycho.videos.VideosHelper.getString;
-
-public class QQ extends BaseExtractor<String> {
+public class QQ extends BaseExtractor<String[]> {
     private static final Pattern MATCH_QQ = Pattern.compile("qq\\.com");
-    public static final String PLAYER_VERSION = "3.2.19.333";
 
     protected QQ(String inputUri, MainActivity mainActivity) {
         super(inputUri, mainActivity);
@@ -29,287 +24,17 @@ public class QQ extends BaseExtractor<String> {
         return false;
     }
 
-    private JSONObject getInfo(String resolution, String vid) {
-        return getObject(String.format("http://vv.video.qq.com/getinfo?otype=json&platform=11&defnpayver=1&appver=%s&defn=%s&vid=%s",
-                "3.2.19.333", resolution, vid));
-    }
-
-    private JSONObject getObject(String uri) {
-        String response = getString(uri, null);
-        if (response == null) {
-            Logger.d(String.format("'%s' is null.", "response"));
-            return null;
-        }
-        String jsonBody = StringShare.substringMax(response, "QZOutputJson=", ";");
-        if (jsonBody == null) {
-            Logger.d(String.format("'%s' is null.", "jsonBody"));
-            return null;
-        }
-        JSONObject obj;
-        try {
-            obj = new JSONObject(jsonBody);
-        } catch (JSONException e) {
-            Logger.d(String.format("getInfo: %s", e.getMessage()));
-            return null;
-        }
-        return obj;
-    }
-
-    private String getVid(String uri) {
-        String vid = StringShare.substringLeast(uri, "/", ".html");
-        if (vid != null && vid.length() == 11) {
-            return vid;
-        }
-        String response = getString(uri, null);
-        if (response == null) {
-            Logger.d(String.format("'%s' is null.", "response"));
-            return null;
-        }
-        String[] patterns = new String[]{
-                "vid=(\\w+)",
-                "vid:\\s*[\"'](\\w+)",
-                "vid\\s*=\\s*[\"']\\s*(\\w+)"
-        };
-        for (int i = 0; i < 3; i++) {
-            Pattern pattern = Pattern.compile(patterns[i]);
-            Matcher matcher = pattern.matcher(response);
-            if (matcher.find()) {
-                vid = matcher.group(1);
-                break;
-            }
-        }
-        if (vid == null) {
-            Logger.d(String.format("'%s' is null.", "vid"));
-            return null;
-        }
-        return vid;
-    }
-
-    private String parseCDN(JSONObject obj) {
-        try {
-            JSONObject vl;
-            if (obj.has("vl")) {
-                vl = obj.getJSONObject("vl");
-            } else {
-                return null;
-            }
-            JSONArray vi;
-            if (vl.has("vi")) {
-                vi = vl.getJSONArray("vi");
-            } else {
-                return null;
-            }
-            JSONObject ul;
-            if (vi.getJSONObject(0).has("ul")) {
-                ul = vi.getJSONObject(0).getJSONObject("ul");
-            } else {
-                return null;
-            }
-            JSONArray ui;
-            if (ul.has("ui")) {
-                ui = ul.getJSONArray("ui");
-            } else {
-                return null;
-            }
-            String url;
-            if (ui.getJSONObject(0).has("url")) {
-                url = ui.getJSONObject(0).getString("url");
-                return url;
-            } else {
-                return null;
-            }
-
-        } catch (Exception ignored) {
-            Logger.d(String.format("parseJSON: %s", ignored));
-        }
-        return null;
-    }
-
-    private int parseFC(JSONObject obj) {
-        try {
-            JSONObject vl;
-            if (obj.has("vl")) {
-                vl = obj.getJSONObject("vl");
-            } else {
-                return 0;
-            }
-            JSONArray vi;
-            if (vl.has("vi")) {
-                vi = vl.getJSONArray("vi");
-            } else {
-                return 0;
-            }
-            JSONObject cl;
-            if (vi.getJSONObject(0).has("cl")) {
-                cl = vi.getJSONObject(0).getJSONObject("cl");
-            } else {
-                return 0;
-            }
-            int fc;
-            if (cl.has("fc")) {
-                fc = cl.getInt("fc");
-                return fc;
-            } else {
-                return 0;
-            }
-
-        } catch (Exception ignored) {
-        }
-        return 0;
-    }
-
-    private String parseFN(JSONObject obj) {
-        try {
-            JSONObject vl;
-            if (obj.has("vl")) {
-                vl = obj.getJSONObject("vl");
-            } else {
-                return null;
-            }
-            JSONArray vi;
-            if (vl.has("vi")) {
-                vi = vl.getJSONArray("vi");
-            } else {
-                return null;
-            }
-            String fn;
-            if (vi.getJSONObject(0).has("fn")) {
-                fn = vi.getJSONObject(0).getString("fn");
-                return fn;
-            } else {
-                return null;
-            }
-
-        } catch (Exception ignored) {
-            Logger.d(String.format("parseJSON: %s", ignored));
-        }
-        return null;
-    }
-
-    private String[][] parseResolutions(JSONObject obj) {
-        try {
-            JSONObject fl;
-            if (obj.has("fl")) {
-                fl = obj.getJSONObject("fl");
-            } else {
-                return null;
-            }
-            JSONArray fi;
-            if (fl.has("fi")) {
-                fi = fl.getJSONArray("fi");
-            } else {
-                return null;
-            }
-            String[][] names = new String[fi.length()][2];
-            for (int i = 0; i < fi.length(); i++) {
-                names[i][0] = fi.getJSONObject(i).getString("name");
-                names[i][1] = Integer.toString(fi.getJSONObject(i).getInt("id"));
-
-            }
-            return names;
-        } catch (Exception ignored) {
-            Logger.d(String.format("parseJSON: %s", ignored));
-        }
-        return null;
-    }
 
     @Override
-    protected String fetchVideoUri(String uri) {
-        // 1.
-        String vid = getVid(uri);
-        if (vid == null) return null;
-        JSONObject obj = getInfo("shd", vid);
-        String cdn = parseCDN(obj);
-        if (cdn == null) {
-            Logger.d(String.format("'%s' is null.", "cdn"));
-            return null;
-        }
-        String[][] names = parseResolutions(obj);
-        String fvKey = parseFvKey(obj);
-        if (names == null) {
-            Logger.d(String.format("'%s' is null.", "names"));
-            return null;
-        }
-        for (String[] name : names) {
-            JSONObject objTmp = getInfo(name[0], vid);
-            String fn = parseFN(objTmp);
-            if (fn == null) {
-                Logger.d(String.format("'%s' is null.", "fn"));
-                return null;
-            }
-            int fc = parseFC(objTmp);
-            if (fc == 0) {
-                fc = 1;
-            }
-            boolean fileNameInValid = false;
-            if (StringShare.count(fn, '.') >= 3) {
-                String[] segments = fn.split("\\.");
-                Pattern pattern = Pattern.compile("^p(\\d{3})$");
-                Matcher matcher = pattern.matcher(segments[1]);
-                if (matcher.find()) {
-                    fileNameInValid = true;
-                }
-            }
-            for (int i = 0; i < fc; i++) {
-                JSONObject keyObject = getObject(String.format("http://vv.video.qq.com/getkey?otype=json&platform=11&appver=%s&filename=%s&format=%s&vid=%s",
-                        PLAYER_VERSION, fn, name[1], vid));
-                String key = parseKey(keyObject);
-                if (key == null) {
-                    key = fvKey;
-                }
-                uri = String.format("%s%s?vkey=%s", cdn, fn, key);
-
-            }
-            Logger.d(String.format("fetchVideoUri: %d %s", fc, fn));
-            //
-        }
-        return null;
+    protected String[] fetchVideoUri(String uri) {
+        return Native.fetchQQ(uri);
     }
 
-    private String parseFvKey(JSONObject obj) {
-        try {
-            JSONObject vl;
-            if (obj.has("vl")) {
-                vl = obj.getJSONObject("vl");
-            } else {
-                return null;
-            }
-            JSONArray vi;
-            if (vl.has("vi")) {
-                vi = vl.getJSONArray("vi");
-            } else {
-                return null;
-            }
-            String fvkey;
-            if (vi.getJSONObject(0).has("fvkey")) {
-                return vi.getJSONObject(0).getString("fvkey");
-            } else {
-                return null;
-            }
-
-        } catch (Exception ignored) {
-            Logger.d(String.format("parseJSON: %s", ignored));
-        }
-        return null;
-    }
-
-    private String parseKey(JSONObject obj) {
-        try {
-            String key;
-            if (obj.has("key")) {
-                return obj.getString("key");
-            } else {
-                return null;
-            }
-
-        } catch (Exception ignored) {
-            Logger.d(String.format("parseJSON: %s", ignored));
-        }
-        return null;
-    }
 
     @Override
-    protected void processVideo(String videoUri) {
+    protected void processVideo(String[] videoUris) {
+        Intent intent = new Intent(mMainActivity, IqiyiActivity.class);
+        intent.putExtra(VideoActivity.EXTRA_PLAYLSIT, videoUris);
+        mMainActivity.startActivity(intent);
     }
 }
-// https://v.qq.com/x/cover/k16928rkrk217zb/z00401l30ys.html
